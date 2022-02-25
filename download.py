9540197f8bd1123548146839b0546c6136382fbe
Ben Fattori <ben.fattori@mail.utoronto.ca>
2022-02-25 13:10:19 -0500

    WIP download script

---
+++ download.py
@@ -0,0 +1,14 @@
+import boto3
+from keys import AWS_ACCESS_KEY, AWS_SECRET_ACCESS_KEY
+
+if __name__ == "__main__":
+    PATH = ''
+    s3 = boto3.client(
+        "s3",
+        aws_access_key_id=AWS_ACCESS_KEY,
+        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    )
+    with open(PATH, "rb") as f:
+        s3.upload_fileobj(
+            f, "openwebtxtbf", f"raw/openwebtxt.tar.xv"
+        )
