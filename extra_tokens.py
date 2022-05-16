63900522d1509684d47d57fe86b89ef1ba1aa940
Ben Fattori <ben.fattori@mail.utoronto.ca>
2022-05-15 22:40:20 -0400

    adding special arxiv tokens

---
+++ extra_tokens.py
@@ -0,0 +1,36 @@
+EXTRA_TOKENS = [
+    "[Abstract]",
+    "[Title]",
+    "Astrophysics]",
+    "[High Energy Physics - Experiment]",
+    "[Mathematical Physics]",
+    "[Physics]",
+    "[Algebraic Geometry]",
+    "[Classical Analysis and ODEs]",
+    "[Differential Geometry]",
+    "[Dynamical Systems]",
+    "[Information Theory]",
+    "[Quantum Physics]",
+    "[Artificial Intelligence]",
+    "[Computational Engineering, Finance, and Science]",
+    "[Computer Science and Game Theory]",
+    "[Cryptography and Security]",
+    "[Machine Learning]",
+    "[Social and Information Networks]",
+    "[Biomolecules]",
+    "[Genomics]",
+    "[Neurons and Cognition]",
+    "[Machine Learning]",
+    "[Statistics]",
+]
+
+from transformers import GPT2Tokenizer  
+
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2_arxiv")
+
+with open('test.txt', 'r') as f:
+    text = f.read()
+
+encoded = tokenizer.encode(text)
+
+print(encoded)
